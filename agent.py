3ecc1e2f2e0946521c14d2023ffa13f2cb472f1f
Thibault Schiesser <thibault.schiesser@epfl.ch>
2024-04-23 20:43:29 +0200

    continue randomAgent + prototype class strcuture for DQN

--- agent.py
+++ agent.py
@@ -38,14 +38,44 @@ class RandomAgent(Agent):
             episode_reward += reward
             state = next_state
             done = terminated or truncated
-            
-        print(f"Episode reward after taking random actions: {episode_reward}")
+        return episode_reward
         
 class DQNAgent(Agent):
     
-    def __init__(self):
+    def __init__(self, batch_size = 64, discount_factor = 0.99):
         super().__init__()
+        self.discount_factor = discount_factor
+        self.batch_size = batch_size
+        
+    def select_action(self, state, action):
+        # e-greedy, that can vary (see doc)
+        raise NotImplementedError
 
+    def Q(self, state, action):
+        # MLP
+        raise NotImplementedError
+    
+    def observe(self, state, action, next_state, reward): #replay buffer
+        current_observation = (state, action, next_state, reward)
+        self.observations.append(current_observation)
+        
+    def update(self):
+        pass
+
+    def run(self):
+        state = self.starting_state
+        done = False
+        episode_reward = 0
+        while not done :
+            action = self.select_action(state)
+            next_state, reward, terminated, truncated, _ = env.step(action)
+
+            self.observe(state, action, next_state, reward)
+            
+            episode_reward += reward
+            state = next_state
+            done = terminated or truncated
+        return episode_reward
 
 class DynaAgent(Agent):
     
