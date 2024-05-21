49234abb41cf10b5c5adcd4ef1ce26bb0b11a1cc
Dani Baciu <danibaciu78@gmail.com>
2024-05-21 12:59:48 +0200

    finish dynaAgent

--- Code/DynaAgent.py
+++ Code/DynaAgent.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 
 class DynaAgent(Agent):
     
-    def __init__(self, env, discr_step = [0.025, 0.005], discount_factor = 0.99, k = 1):
+    def __init__(self, env, discr_step = [0.025, 0.005], discount_factor = 0.99, k_updates = 100):
         super().__init__(env)
 
         # Environment values
@@ -18,7 +18,7 @@ class DynaAgent(Agent):
         self.interval_speed = [-0.07, 0.007]
 
         # Initialization variables
-        self.k = k
+        self.k_updates = k_updates
         self.discr_step = discr_step
         self.discount_factor = discount_factor
 
@@ -32,13 +32,12 @@ class DynaAgent(Agent):
         self.discretization_speed = np.linspace(self.interval_speed[0], self.interval_speed[1], self.nb_interval_speed + 1)
 
         # Tabular values :
-        self.P_estimate = np.ones((self.nb_states, self.nb_actions, self.nb_states))*(1/self.nb_states) #uniform at initialization
+        self.P_estimate = np.ones((self.nb_states, self.nb_actions, self.nb_states)) / (self.nb_states * self.nb_actions)
         self.R_estimate = np.zeros((self.nb_states, self.nb_actions))
         self.Q = np.zeros((self.nb_states, self.nb_actions))
         
         
     def found_indice_bin(self, state):
-        
         # Extract position and speed form state
         position = state[0]
         speed = state[1]
@@ -74,13 +73,47 @@ class DynaAgent(Agent):
         indice_state = self.found_indice_bin(state) 
         
         if np.random.uniform(0, 1) > epsilon:
-            best_action = np.max(self.Q[indice_state,:])
-            return best_action
+            return np.max(self.Q[indice_state,:])
         else :
-            random_action = np.random.choice(self.Q[indice_state,:])# choose a random action
-            return random_action
+            return np.random.choice(self.Q[indice_state,:])
     
-    
-    def run():
-        pass
-                
+        
+    def observe(self, state, action, next_state, reward, learning_rate):
+        index_s = self.found_indice_bin(state)
+        index_s_prime = self.found_indice_bin(next_state)
+        self.R_estimate[index_s][action] = (1 - learning_rate) * self.R_estimate[index_s][action] + learning_rate * reward
+        self.P_estimate[index_s][action][index_s_prime] = (1 - learning_rate) * self.P_estimate[index_s][action][index_s_prime] + learning_rate
+        
+    def update(self, iteration_number, starting_epsilon = 0.8, ending_epsilon = 0.05, epsilon_decay = 150):
+        for _ in range(self.k_updates):
+            s = np.random.randint(self.nb_states)
+            a = np.random.randint(self.nb_actions)
+            s_prime_probs = self.P_estimate[s][a]
+            expected_reward = self.R_estimate[s][a]
+            future_rewards = np.max(self.Q, axis=-1)
+            expected_future_reward = np.sum(s_prime_probs * future_rewards)
+            self.Q[s][a] = expected_reward + self.discount_factor * expected_future_reward
+
+            # epsilon = ending_epsilon + (starting_epsilon - ending_epsilon) * math.exp(-iteration_number/epsilon_decay)
+
+
+    def run(self, num_episodes = 3000, learning_rate = 0.005, starting_epsilon = 0.8, ending_epsilon = 0.05, epsilon_decay = 150):
+        for episode in range(num_episodes):
+            state = self.env.reset()
+            done = False
+            while not done:
+                action = self.select_action(state=state, 
+                                            iteration_number=episode, 
+                                            starting_epsilon=starting_epsilon, 
+                                            ending_epsilon=ending_epsilon, 
+                                            epsilon_decay=epsilon_decay
+                                            )
+                next_state, reward, done, _ = self.env.step(action)
+                self.observe(state, action, next_state, reward, learning_rate)
+                self.update(iteration_number=episode,
+                            starting_epsilon=starting_epsilon, 
+                            ending_epsilon=ending_epsilon, 
+                            epsilon_decay=epsilon_decay
+                            )
+                state = next_state
+            print(f"Episode {episode + 1} completed.")
