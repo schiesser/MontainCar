ec500db16b96d9ce8a9b3863a9b49c5c3de0e638
Thibault Schiesser <thibault.schiesser@epfl.ch>
2024-05-28 16:46:39 +0200

    update

--- Code/DQN.py
+++ Code/DQN.py
@@ -4,12 +4,12 @@ import torch.nn.functional as F
 
 class DQN(nn.Module):
 
-    def __init__(self, info_per_state = 2, number_actions = 3):
+    def __init__(self, info_per_state = 2, number_actions = 3, nb_neurons=64):
         super(DQN, self).__init__()
         
-        self.layer1 = nn.Linear(info_per_state, 64)
-        self.layer2 = nn.Linear(64, 64)
-        self.layer3 = nn.Linear(64, number_actions)
+        self.layer1 = nn.Linear(info_per_state, nb_neurons)
+        self.layer2 = nn.Linear(nb_neurons, nb_neurons)
+        self.layer3 = nn.Linear(nb_neurons, number_actions)
         
     def forward(self, x):
         
@@ -19,12 +19,12 @@ class DQN(nn.Module):
 
 class RND(nn.Module):
 
-    def __init__(self, info_per_state = 2, nb_neurons = 16):
+    def __init__(self, info_per_state = 2, nb_neurons=32):
         super(RND, self).__init__()
 
-        self.layer1 = nn.Linear(info_per_state, 32)
-        self.layer2 = nn.Linear(32, 32)
-        self.layer3 = nn.Linear(32, 1)
+        self.layer1 = nn.Linear(info_per_state, nb_neurons)
+        self.layer2 = nn.Linear(nb_neurons, nb_neurons)
+        self.layer3 = nn.Linear(nb_neurons, 1)
         
     def forward(self, x):
         
